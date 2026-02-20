c2267d3c9e79d71affae48e8694206958decee4f
Casper Andersson <casper.casan@gmail.com>
2026-02-20 22:34:10 +0100

    Start migrating tstest pkt to here

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	//"bytes"
 	"encoding/binary"
+	"os"
+
 	//"encoding/hex"
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"syscall"
 	"time"
-	"flag"
 
 	"golang.org/x/sys/unix"
 
@@ -39,8 +41,49 @@ type Port struct {
 	EFd       int
 }
 
+type PacketData struct {
+	packet   ptp.Packet
+	rxtstamp time.Time
+}
+
+type Options struct {
+	transportSpecific uint8
+	twoStepFlag_set   bool
+	human_readable    bool
+	ingressLatency    uint64
+	egressLatency     uint64
+	header_offset     uint
+	nonstop_flag      bool
+	twoStepFlag       bool
+	interval          time.Duration
+	pkttype           *ptp.MessageType
+	minor_version     uint8
+	domain            uint8
+	count             int
+	vlan              *int
+	prio              int
+	seq               uint16
+
+	rx_mode    bool
+	iface      string
+	delay_mode string
+	clk_type   string
+	udp        bool
+
+	// tstamp_all        int
+	// auto_fup          int
+	// listen   int
+	// unsigned char mac[ETH_ALEN];
+	// char *interface;
+	// int sequence_types[SEQUENCE_MAX];
+	// int sequence_length;
+	// enum delay_mechanism dm;
+	// enum hwtstamp_clk_types clk_type;
+
+}
+
 func htons(i uint16) uint16 {
-    return (i<<8)&0xff00 | i>>8
+	return (i<<8)&0xff00 | i>>8
 }
 
 func (p *Port) openSocket() error {
@@ -55,16 +98,16 @@ func (p *Port) openSocket() error {
 		}
 		p.Interface = interf
 		p.EFd = eFd
-		
+
 		sll := &syscall.SockaddrLinklayer{
-		Protocol: htons(syscall.ETH_P_ALL),
-		Ifindex:  interf.Index,
+			Protocol: htons(syscall.ETH_P_ALL),
+			Ifindex:  interf.Index,
 		}
 		if err := syscall.Bind(eFd, sll); err != nil {
 			log.Fatalf("bind to %s failed: %v", p.IfaceStr, err)
-    		}
+		}
 
-		fmt.Printf("Opened L2 socket on %s\n", p.IfaceStr)
+		// fmt.Printf("Opened L2 socket on %s\n", p.IfaceStr)
 	} else if p.Layer == LayerUDPv4 {
 		eventConn, err := net.ListenUDP("udp", &net.UDPAddr{IP: p.IP, Port: event_port})
 		if err != nil {
@@ -108,103 +151,137 @@ func (port *Port) transmit(syncP *ptp.SyncDelayReq) error {
 	return err
 }
 
-func (port *Port) receive() error {
-	buf := make([]byte, timestamp.PayloadSizeBytes)
-	oob := make([]byte, timestamp.ControlSizeBytes)
-	// toob := make([]byte, timestamp.ControlSizeBytes)
-
+func (port *Port) receive(buf []byte, oob []byte) (*PacketData, error) {
 	switch port.Layer {
 	case LayerMac:
-		
-		//n, from, err := syscall.Recvfrom(port.EFd, buf, 0)
-		//if err != nil {
-			//log.Fatalf("recvfrom failed: %v", err)
-		//}
-		//fmt.Printf("Received %d bytes from %+v\n", n, from)
-		//fmt.Println(hex.Dump(buf[:n]))
-
-		//_, _, _, saddr, err := unix.Recvmsg(port.EFd, buf, oob, 0)
-		//if err != nil {
-			//return err
-		//}
-		//fmt.Printf("saddr %v\n", saddr)
-		//_, _, rxTS, err := timestamp.ReadPacketWithRXTimestampBuf(port.EFd, buf, oob)
-		//data, returnaddr, nowKernelTimestamp, err := ReadPacketWithRXTimestamp(connFd)
-		_, _, rxTS, err := timestamp.ReadPacketWithRXTimestamp(port.EFd)
+		bytes, _, rxTS, err := timestamp.ReadPacketWithRXTimestampBuf(port.EFd, buf, oob)
+		p, err := ptp.DecodePacket(buf[14:bytes])
 		if err != nil {
-			return err
+			return nil, err
 		}
-		fmt.Println("RX TS:", rxTS.UnixNano())
-		//log.Fatal("receive: not implemented")
+		data := &PacketData{
+			packet:   p,
+			rxtstamp: rxTS,
+		}
+		return data, nil
 	case LayerUDPv4:
 		_, _, rxTS, err := timestamp.ReadPacketWithRXTimestampBuf(port.EFd, buf, oob)
+		// TODO: Parse packet
 		if err != nil {
-			return err
+			return nil, err
 		}
 		fmt.Println("RX TS:", rxTS.UnixNano())
 	default:
 		log.Fatal("receive: not implemented")
 	}
-	return nil
+	return nil, nil
 }
 
-func main() {
-	// port := 319
-	// dest := net.IPv4(10, 11, 0, 2)
-	//use_l2 := true
-	var (
-		// eFd    int
-		// err    error
-		// interf *net.Interface
-		port Port
-		txTS time.Time
-	)
-	p1 := false
+func (port *Port) rx_mode(opts *Options, c chan PacketData) error {
 
-	var txport = flag.String("tx", "", "help message for flag n")
-	var rxport = flag.String("rx", "", "help message for flag n")
-	var l2 = flag.Bool("2", false, "help message for flag n")
+	buf := make([]byte, timestamp.PayloadSizeBytes)
+	oob := make([]byte, timestamp.ControlSizeBytes)
+	for {
+		pd, err := port.receive(buf, oob)
+		if err != nil {
+			continue
+		}
+		c <- *pd
+	}
+}
 
-	flag.Parse()
+func (port *Port) tx_mode(opts *Options) {
+	syncP := &ptp.SyncDelayReq{
+		Header: ptp.Header{
+			SdoIDAndMsgType: ptp.NewSdoIDAndMsgType(ptp.MessageSync, 0),
+			Version:         ptp.Version,
+			MessageLength:   uint16(binary.Size(ptp.Header{}) + binary.Size(ptp.SyncDelayReqBody{})), //#nosec G115
+			DomainNumber:    opts.domain,
+			FlagField:       ptp.FlagTwoStep,
+			SequenceID:      opts.seq,
+			SourcePortIdentity: ptp.PortIdentity{
+				PortNumber:    1,
+				ClockIdentity: 0x000000fffeaa0000,
+			},
+			LogMessageInterval: 0,
+			ControlField:       0,
+		},
+	}
 
-	fmt.Printf("TX: %s. RX: %s. L2: %v\n", *txport, *rxport, *l2)
+	oob := make([]byte, timestamp.ControlSizeBytes)
+	toob := make([]byte, timestamp.ControlSizeBytes)
+	for _ = range opts.count {
+		err := port.transmit(syncP)
+		syncP.Header.SequenceID += 1
+		if err != nil {
+			continue
+		}
+		txTS, _, err := timestamp.ReadTXtimestampBuf(port.EFd, oob, toob)
+		if err != nil {
+			continue
+		}
+		fmt.Println(txTS.UnixNano())
+		time.Sleep(opts.interval)
+	}
+}
 
-	if *txport == "" && *rxport == "" {
+func pkt_mode(args []string) {
+	var port Port
+	var opts = Options{}
+
+	fs := flag.NewFlagSet("pkt", flag.ContinueOnError)
+	fs.StringVar(&opts.iface, "if", "", "Interface name to operate on")
+	fs.BoolVar(&opts.rx_mode, "r", false, "Receive mode")
+	fs.BoolVar(&opts.udp, "4", false, "Use UDP instead of L2")
+	fs.IntVar(&opts.count, "c", 1, "Number of packets to transmit")
+	fs.Uint64Var(&opts.ingressLatency, "ilat", 0, "Ingress latency")
+	fs.Uint64Var(&opts.egressLatency, "elat", 0, "Egress latency")
+	var interval = fs.Uint("interval", 1000, "TX packet interval (ms)")
+	var domain = fs.Int("domain", 0, "PTP domain")
+	var seq = fs.Int("seq", 0, "Starting SequenceID")
+
+	err := fs.Parse(args)
+
+	if err != nil {
 		return
 	}
 
-	if *txport != "" {
-		p1 = true
+	if opts.iface == "" {
+		fmt.Println("Must specify interface with -if")
+		return
 	}
+	if *domain > 255 {
+		fmt.Println("Domain must be 0-255")
+		return
+	}
+	if *interval < 0 {
+		fmt.Println("Interval must be >= 0")
+		return
+	}
+	opts.domain = uint8(*domain)
+	opts.interval = time.Duration(*interval) * time.Millisecond
+	opts.seq = uint16(*seq)
+	// fmt.Printf("%v\n", opts)
+	// fmt.Printf("Port %s\n", opts.iface)
 
-
-	if p1 {
-		ip := net.IPv4(10, 11, 0, 1)
-		// dest := net.IPv4(224, 0, 1, 129)
-		dest := net.IPv4(10, 11, 0, 2)
-		iface := *txport
-		if *l2 {
-			port.IfaceStr = iface
-			port.Layer = LayerMac
+	if opts.udp {
+		var ip net.IP
+		var dest net.IP
+		if !opts.rx_mode {
+			ip = net.IPv4(10, 11, 0, 1)
+			// dest := net.IPv4(224, 0, 1, 129)
+			dest = net.IPv4(10, 11, 0, 2)
 		} else {
-			port.IfaceStr = iface
-			port.Layer = LayerUDPv4
-			port.IP = ip
-			port.DestIP = dest
+			ip = net.IPv4(10, 11, 0, 2)
+			dest = net.IPv4(10, 11, 0, 1)
 		}
+		port.IfaceStr = opts.iface
+		port.Layer = LayerUDPv4
+		port.IP = ip
+		port.DestIP = dest
 	} else {
-		ip := net.IPv4(10, 11, 0, 2)
-		dest := net.IPv4(10, 11, 0, 1)
-		iface := *rxport
-		if *l2 {
-			port.IfaceStr = iface
-			port.Layer = LayerMac
-		} else {
-			port.IfaceStr = iface
-			port.Layer = LayerUDPv4
-			port.IP = ip
-			port.DestIP = dest
-		}
+		port.IfaceStr = opts.iface
+		port.Layer = LayerMac
 	}
 	//fmt.Println("Hello, World!")
 	// if use_l2 {
@@ -220,7 +297,7 @@ func main() {
 	// 	eFd, err = timestamp.ConnFd(eventConn)
 	// }
 
-	err := port.openSocket()
+	err = port.openSocket()
 
 	if err != nil {
 		log.Fatalf("Getting event connection FD: %s", err)
@@ -236,39 +313,34 @@ func main() {
 		log.Fatalf("Failed to set socket to blocking: %s", err)
 	}
 
-	syncP := &ptp.SyncDelayReq{
-		Header: ptp.Header{
-			SdoIDAndMsgType: ptp.NewSdoIDAndMsgType(ptp.MessageSync, 0),
-			Version:         ptp.Version,
-			MessageLength:   uint16(binary.Size(ptp.Header{}) + binary.Size(ptp.SyncDelayReqBody{})), //#nosec G115
-			DomainNumber:    0,
-			FlagField:       ptp.FlagTwoStep,
-			SequenceID:      0,
-			SourcePortIdentity: ptp.PortIdentity{
-				PortNumber:    1,
-				ClockIdentity: 0x000000fffeaa0000,
-			},
-			LogMessageInterval: 0,
-			ControlField:       0,
-		},
-	}
-	oob := make([]byte, timestamp.ControlSizeBytes)
-	// TMP buffers
-	toob := make([]byte, timestamp.ControlSizeBytes)
+	if opts.rx_mode {
+		c := make(chan PacketData)
 
-	if p1 {
-		err = port.transmit(syncP)
-		txTS, _, err = timestamp.ReadTXtimestampBuf(port.EFd, oob, toob)
-		fmt.Println(txTS.UnixNano())
+		go port.rx_mode(&opts, c)
+
+		for pd := range c {
+			msgtype := pd.packet.MessageType()
+			sync, ok := pd.packet.(*ptp.SyncDelayReq)
+			if !ok {
+				fmt.Println("Received packet was not a Sync")
+			}
+			rx_ns := pd.rxtstamp.UnixNano() % 1000000000
+			rx_s := pd.rxtstamp.Unix()
+			seq := sync.Header.SequenceID
+			corr := sync.Header.CorrectionField.Duration()
+			domain := sync.Header.DomainNumber
+			fmt.Printf("%s | Seq %d | Dom %d | RXts %d.%09d | Corr %d\n", msgtype, seq, domain, rx_s, rx_ns, corr)
+		}
 	} else {
-		port.receive()
+		port.tx_mode(&opts)
 	}
+}
 
-	// err = unix.Sendto(eFd, buf[:n], 0, eclisa)
-	// txTS, _, err = timestamp.ReadTXtimestampBuf(eFd, oob, toob)
-	// fmt.Println(txTS.UnixNano())
-
-	// err = unix.Sendto(eFd, buf[:n], 0, eclisa)
-	// txTS, _, err = timestamp.ReadTXtimestampBuf(eFd, oob, toob)
-	// fmt.Println(txTS.UnixNano())
+func main() {
+	mode := os.Args[1]
+	args := os.Args[2:]
+	if mode == "pkt" {
+		pkt_mode(args)
+		return
+	}
 }
