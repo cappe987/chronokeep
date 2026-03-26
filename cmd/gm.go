1437189029de5147554b310973016f921203dcef
Casper Andersson <casper.casan@gmail.com>
2026-03-26 23:32:41 +0100

    Add GM mode. Forgot to commit the file itself earlier

---
+++ cmd/gm.go
@@ -0,0 +1,103 @@
+package cmd
+
+import (
+	. "intime/internal"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	ptp "github.com/facebook/time/ptp/protocol"
+	timestamp "github.com/facebook/time/timestamp"
+	"github.com/pborman/getopt/v2"
+)
+
+func GmMode() {
+	var port Port
+	var opts = CommonOpts{}
+	interval_ms := uint32(1000)
+	pdelayMode := false
+
+	opts.DefineCommonFlags()
+	opts.RecordPackets = false
+	getopt.FlagLong(&interval_ms, "interval", 'I', "TX packet interval (ms)")
+	getopt.FlagLong(&pdelayMode, "peertopeer", 'P', "P2P Mode")
+	getopt.Parse()
+	opts.Validate()
+	Interval := time.Duration(interval_ms) * time.Millisecond
+
+	err := port.Init(opts, 0, 1)
+	if err != nil {
+		log.Fatalf("Failed initializing port: %s", err)
+	}
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	rxCh := make(chan PacketData, 100)
+	quit := make(chan int)
+	var ticker *time.Ticker
+	running := true
+	seq := uint16(0)
+
+	ticker = time.NewTicker(Interval)
+	gmTxPackets(&port, &seq, pdelayMode)
+
+	go port.RxMode(rxCh, quit)
+	for running {
+		select {
+		case <-sigs:
+			quit <- 0
+			running = false
+		case pd := <-rxCh:
+			pd.Print()
+			replyToDelay(&port, &pd, pdelayMode)
+		case <-ticker.C:
+			gmTxPackets(&port, &seq, pdelayMode)
+		}
+	}
+	// TODO: Requires HW to test
+	timestamp.DisableTimestamps(port.EFd, port.Interface)
+}
+
+func replyToDelay(port *Port, pd *PacketData, pdelayMode bool) {
+	if !pdelayMode && pd.IsDelayReq() {
+		resp := port.MakeResponseDelay(pd)
+		port.Transmit(resp)
+		resp.Print()
+	} else if pdelayMode && pd.IsPDelayReq() {
+		resp := port.MakeResponsePDelay(pd)
+		port.Transmit(resp)
+		resp.Print()
+		respFup := port.MakeFollowUpPDelay(resp)
+		port.Transmit(respFup)
+		respFup.Print()
+	}
+}
+
+func gmTxPackets(port *Port, seq *uint16, pdelayMode bool) {
+
+	anno := port.BuildAnnounce(*seq)
+	port.Transmit(anno)
+	anno.Print()
+
+	sync, err := port.BuildPacket(ptp.MessageSync, *seq)
+	if err != nil {
+		log.Fatalf("Failed building packet: %s", err)
+	}
+	port.Transmit(sync)
+	sync.Print()
+	fup, err := port.MakeFollowUp(sync)
+	if err == nil {
+		port.Transmit(fup)
+		fup.Print()
+	}
+
+	if pdelayMode {
+		pdelayReq := port.BuildPDelayReq(*seq)
+		port.Transmit(pdelayReq)
+		pdelayReq.Print()
+	}
+
+	*seq += uint16(1)
+}
--- internal/common.go
+++ internal/common.go
@@ -29,9 +29,9 @@ type CommonOpts struct {
 }
 
 func (opts *CommonOpts) DefineCommonFlags() {
-	opts.DestIp = "224.0.1.129"    // TODO: 224.0.0.107 for pdelays
-	opts.Mac = "01:1b:19:00:00:00" // TODO: 01:80:c2:00:00:0e for pdelays
-	opts.RecordPackets = true      // Always true for now
+	opts.DestIp = "224.0.1.129" // TODO: 224.0.0.107 for pdelays
+	opts.Mac = "01:1b:19:00:00:00"
+	opts.RecordPackets = true
 
 	getopt.FlagLong(&opts.Domain, "domain", 'd', "PTP domain")
 	getopt.FlagLong(&opts.Iface, "iface", 'i', "Interface to operate on")
